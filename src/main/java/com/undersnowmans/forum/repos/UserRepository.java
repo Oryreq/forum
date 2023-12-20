fd57516d172760d569204d7d50fbfac9194b1256
Vsevolod <84316448+Oryreq@users.noreply.github.com>
2023-12-21 03:41:28 +0400

    Repos added

--- src/main/java/com/undersnowmans/forum/repos/UserRepository.java
+++ src/main/java/com/undersnowmans/forum/repos/UserRepository.java
@@ -1,12 +1,12 @@
 package com.undersnowmans.forum.repos;
 
 import com.undersnowmans.forum.models.User;
-import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
 import java.util.Optional;
 
 @Repository
-public interface UserRepository extends JpaRepository<User, Long> {
+public interface UserRepository extends CrudRepository<User, Long> {
     Optional<User> findUserByUsername(String username);
 }
