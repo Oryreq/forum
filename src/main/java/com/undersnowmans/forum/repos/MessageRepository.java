23a3dfbdfbd154af940f0fea95e47f5e60b77083
Vsevolod <ash.seva@gmail.com>
2023-12-21 06:08:58 +0400

    Update controllers

--- src/main/java/com/undersnowmans/forum/repos/MessageRepository.java
+++ src/main/java/com/undersnowmans/forum/repos/MessageRepository.java
@@ -3,5 +3,9 @@ package com.undersnowmans.forum.repos;
 import com.undersnowmans.forum.models.Message;
 import org.springframework.data.repository.CrudRepository;
 
+import java.util.List;
+
 public interface MessageRepository extends CrudRepository<Message, Long> {
+    @Override
+    List<Message> findAll();
 }
--- src/main/java/com/undersnowmans/forum/repos/ThreadRepository.java
+++ src/main/java/com/undersnowmans/forum/repos/ThreadRepository.java
@@ -3,5 +3,10 @@ package com.undersnowmans.forum.repos;
 import com.undersnowmans.forum.models.Thread;
 import org.springframework.data.repository.CrudRepository;
 
+import java.util.List;
+
 public interface ThreadRepository extends CrudRepository<Thread, Long> {
+
+    @Override
+    List<Thread> findAll();
 }
